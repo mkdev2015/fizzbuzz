d22510c6fa7ce7537e7c3e6db80a0217fada046f
Matilda <matildakond@gmail.com>
2016-09-20 01:30:15 +0000

    Made changes, finally following assignment instructions and starting the for loop at 1 instead of 0 which was the reason why my code did not pass the test. Also attempted the extra credit. Does not seem to work though.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 
-for(var i= 0;i <= 100; i++){
+for(var i= 1;i <= 100; i++){
 
     if(i % 3 == 0 && i % 5 == 0)
         console.log('FizzBuzz');
@@ -10,5 +10,12 @@ for(var i= 0;i <= 100; i++){
     else
         console.log(i);
 }
-        
- 
+
+function fizzbuzz(max){
+    if (max <= i)
+    return i;
+    
+    console.log(max);
+}
+
+fizzbuzz(5);
